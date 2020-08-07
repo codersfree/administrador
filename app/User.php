60cbc5ccacc3923987b792c589e326e77638e2ee
vhaf123 <victor.aranaf92@gmail.com>
2020-08-07 13:13:08 -0500

    subida de proyecto

---
+++ app/User.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function adminlte_image(){
+
+        return 'https://picsum.photos/300/300';
+        
+    }
+
+    public function adminlte_desc(){
+        return "Administrador";
+    }
+
+    public function adminlte_profile_url()
+    {
+        return 'profile/username';
+    }
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,83 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Tablero</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, et obcaecati autem magnam explicabo repellat distinctio, similique accusamus, maiores in praesentium voluptate atque non magni at iste perferendis sint fugiat?</p>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, et obcaecati autem magnam explicabo repellat distinctio, similique accusamus, maiores in praesentium voluptate atque non magni at iste perferendis sint fugiat?</p>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, et obcaecati autem magnam explicabo repellat distinctio, similique accusamus, maiores in praesentium voluptate atque non magni at iste perferendis sint fugiat?</p>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, et obcaecati autem magnam explicabo repellat distinctio, similique accusamus, maiores in praesentium voluptate atque non magni at iste perferendis sint fugiat?</p>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, et obcaecati autem magnam explicabo repellat distinctio, similique accusamus, maiores in praesentium voluptate atque non magni at iste perferendis sint fugiat?</p>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, et obcaecati autem magnam explicabo repellat distinctio, similique accusamus, maiores in praesentium voluptate atque non magni at iste perferendis sint fugiat?</p>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        Swal.fire(
+        'Good job!',
+        'You clicked the button!',
+        'success'
+        )
+    </script>
+@stop
